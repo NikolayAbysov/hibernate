0dffabe891cdcfe58c07e48efb5d3fc06237e2fc
NikolayAbysov <nikolay.abysov@ukr.net>
2020-05-22 10:18:40 +0300

    first commit

---
+++ src/main/java/com/dev/hibernate/lib/anno/Dao.java
@@ -0,0 +1,8 @@
+package com.dev.hibernate.lib.anno;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Dao {
+}
